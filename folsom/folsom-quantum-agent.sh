f632def107e79bb196f433e5ac49c73f869fed9d
Emiliano Berenbaum <eberenbaum@mobileiron.com>
2013-05-29 16:40:52 -0700

    simple change

--- folsom/folsom-quantum-agent.sh
+++ folsom/folsom-quantum-agent.sh
@@ -38,8 +38,8 @@ function quantum_agent_configure() {
     sudo sed -i "s/^# fake_rabbit.*/fake_rabbit = False/g"              ${QUANTUM_CONF}
     sudo set -i "s/^debug =.*/debug = False/g"                          ${QUANTUM_CONF}
 
-    sudo sed -i "s/^# rabbit_host.*/rabbit_host = ${RABBIT_ENDPOINT}/g" ${QUANTUM_CONF}
-    sudo sed -i "s/^# rabbit_port.*/rabbit_port = ${RABBIT_PORT}/g"     ${QUANTUM_CONF}
+    sudo sed -i "s/^#\d*rabbit_host.*/rabbit_host = ${RABBIT_ENDPOINT}/g" ${QUANTUM_CONF}
+    sudo sed -i "s/^#\d*rabbit_port.*/rabbit_port = ${RABBIT_PORT}/g"     ${QUANTUM_CONF}
 
     #===========================
     # ovs_quantum_plugin.ini
